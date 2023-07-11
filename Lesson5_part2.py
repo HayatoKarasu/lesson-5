34e0fe1f61c2e8d7bac0ab7b7d50c88261a2f80c
HayatoKarasu <138600849+HayatoKarasu@users.noreply.github.com>
2023-07-11 21:33:40 +0700

    Add files via upload

---
+++ Lesson5_part2.py
@@ -0,0 +1,17 @@
+﻿word = str(input("Введите любое слово на английском: "))
+
+glas = 'aeiou'
+gl = 0
+
+b = len(word)
+
+for c in word:
+    if c in glas:
+        gl += 1
+        print(c, end = ' ')
+
+if gl > 0:
+    d = b - gl 
+    print("В данном слове", gl, "гласных и", d, "согласных")
+elif gl == 0:
+    print("False")
---
+++ Lesson5_part3.py
@@ -0,0 +1,10 @@
+﻿a = int(input("Сколько денег у Ивана: "))
+b = int(input("Сколько денег у Майкла: "))
+x = int(input("Минимум для вложений: "))
+
+if (a >= x) and (b >= x):
+    print(2)
+elif ((a < x) and (b >= x)) or ((b < x) and (a >= x)):
+    print(1)
+else:
+    print(0)
